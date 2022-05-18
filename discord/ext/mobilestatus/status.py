3d6952d8b2155e9028d86b96ad669888ecf9e9ba
EpiclyRaspberry <raspberryepicly@gmail.com>
2022-05-19 04:37:24 +0800

    innit

---
+++ discord/ext/mobilestatus/status.py
@@ -0,0 +1,42 @@
+import discord
+
+@property
+async def identify(self) -> None:
+        
+        payload = {
+            'op': self.IDENTIFY,
+            'd': {
+                'token': self.token,
+                'properties': {
+                    '$os': "android",
+                    '$browser': 'Discord Android',
+                    '$device': 'discord.py',
+                    '$referrer': '',
+                    '$referring_domain': '',
+                },
+                'compress': True,
+                'large_threshold': 250,
+            },
+        }
+
+        if self.shard_id is not None and self.shard_count is not None:
+            payload['d']['shard'] = [self.shard_id, self.shard_count]
+
+        state = self._connection
+        if state._activity is not None or state._status is not None:
+            payload['d']['presence'] = {
+                'status': state._status,
+                'game': state._activity,
+                'since': 0,
+                'afk': False,
+            }
+
+        if state._intents is not None:
+            payload['d']['intents'] = state._intents.value
+
+        await self.call_hooks('before_identify', self.shard_id, initial=self._initial_identify)
+        await self.send_as_json(payload)
+        _log.info('Shard ID %s has sent the IDENTIFY payload.', self.shard_id)
+
+
+discord.gateway.DiscordVoiceWebSocket.identify = identify
