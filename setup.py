d08eaaf6b88a2cf5f9192cd3417261ec936c9d83
EpiclyRaspberry <raspberryepicly@gmail.com>
2022-05-19 04:37:13 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="discord-ext-mobilestatus",
+    description="Sets the bot's status to mobile instead of web",
+    version="0.0.1",
+    author="EpiclyRaspberry",
+    author_email="raspberryepicly@gmail.com",
+    packages=["discord.ext.mobilestatus"],
+    url="https://github.com/EpiclyRaspberry/discord-ext-mobilestatus",
+    install_requires=["discord.py"]
+)
+
